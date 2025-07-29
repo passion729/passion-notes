c58e6fce502df271d1e0f0c5e35d45d1e67adf28
Passion <oscarpassion36@gmail.com>
2025-07-29 16:54:01 +0800

    fix: fix style problem while on deploy and remove tailwindcss temporary

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -9,28 +9,19 @@ const config: Config = {
     tagline: 'Dinosaurs are cool',
     favicon: 'img/Snoopy.png',
     // themes: ['@docusaurus/theme-search-algolia'],
-    plugins: ["./src/plugins/tailwind-config.ts"],
     // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
     future: {
         v4: true, // Improve compatibility with the upcoming Docusaurus v4
     },
 
     // Set the production url of your site here
-    url: 'https://your-docusaurus-site.example.com',
+    url: 'https://notes.passion7.tech',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
     baseUrl: '/',
 
     onBrokenLinks: 'throw',
     onBrokenMarkdownLinks: 'warn',
-
-    // Even if you don't use internationalization, you can use this field to set
-    // useful metadata like html lang. For example, if your site is Chinese, you
-    // may want to replace "en" with "zh-Hans".
-    i18n: {
-        defaultLocale: 'en',
-        locales: ['en'],
-    },
 
     presets: [
         [
