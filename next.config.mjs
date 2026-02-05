100f1be5a2d8f8e8fabb7370bc75414b9b876001
Passion <oscarpassion36@gmail.com>
2026-02-06 04:09:57 +0800

    Revert "build: switch to next static build"
    This reverts commit 232933c25763aa3d74c46d1c12dfb4e93b5f1213.

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,18 @@
-import { createMDX } from "fumadocs-mdx/next";
+import { createMDX } from 'fumadocs-mdx/next';
 
 const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
-    output: "export",
-    reactStrictMode: true,
-    async rewrites() {
-        return [
-            {
-                source: "/docs/:path*.mdx",
-                destination: "/llms.mdx/docs/:path*",
-            },
-        ];
-    },
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/docs/:path*.mdx',
+        destination: '/llms.mdx/docs/:path*',
+      },
+    ];
+  },
 };
 
 export default withMDX(config);
